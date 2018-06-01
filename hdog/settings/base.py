a7fac2d5bc4469ec7831d3872a9993ebea3b1b64
Golikov Ivan <reg@eml.cc>
2018-06-01 13:26:09 +0300

    Создание моделей БД (#14)

--- hdog/settings/base.py
+++ hdog/settings/base.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'category',
     'hdog',
 ]
 
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1

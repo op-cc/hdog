a9557a5b10436e91eb1a0fb44f1cb8d7fc37f3c2
Golikov Ivan <ivnglkv@eml.cc>
2018-06-01 17:51:37 +0300

    Админ-класс для складов

--- hdog/admin.py
+++ hdog/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import (
     Measure,
+    Stock,
 )
 
 
 @admin.register(Measure)
 class MeasureAdmin(admin.ModelAdmin):
     list_display = ('name', 'national_symbol', 'key')
+
+
+admin.site.register(Stock)

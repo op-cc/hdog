f9dde5444c16646913b8b87600973171bdab7027
Golikov Ivan <reg@eml.cc>
2018-06-04 09:33:05 +0300

    Исправлена миграция (#15)

--- hdog/migrations/0003_auto_20180601_1240.py
+++ hdog/migrations/0003_auto_20180601_1240.py
@@ -8,8 +8,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('category', '0002_auto_20180601_1240'),
-        ('contenttypes', '0002_remove_content_type_name'),
         ('hdog', '0002_auto_20180528_1354'),
     ]
 

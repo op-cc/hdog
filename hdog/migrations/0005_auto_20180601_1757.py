f7d80e1c4103b50a2c329e094ab57af8a5e06089
Golikov Ivan <ivnglkv@eml.cc>
2018-06-04 23:13:05 +0300

    Исправлена миграция после мержа

--- hdog/migrations/0005_auto_20180601_1757.py
+++ hdog/migrations/0005_auto_20180601_1757.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hdog', '0004_auto_20180601_1308'),
+        ('hdog', '0001_initial'),
     ]
 
     operations = [
